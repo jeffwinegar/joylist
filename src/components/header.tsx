14bc0f2896a0444d642b02ddd8e46ea0182e8200
Jeff Winegar <winegarj@gmail.com>
2023-08-13 17:59:24 -0700

    feat: add custom userButton with link to profile page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,120 @@
 import {
+  ClerkLoaded,
   SignInButton,
+  SignOutButton,
   SignUpButton,
   SignedIn,
   SignedOut,
-  UserButton,
+  useClerk,
+  useUser,
 } from '@clerk/nextjs';
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import styles from './header.module.css';
+import * as Popover from '@radix-ui/react-popover';
+
+const CustomUserButton = () => {
+  const { user } = useUser();
+  const { session, openUserProfile } = useClerk();
+
+  if (!user) return;
+  // required in Clerk so these should never be empty
+  const firstName = user.firstName ?? '';
+  const lastName = user.lastName ?? '';
+
+  return (
+    <ClerkLoaded>
+      <Popover.Root>
+        <Popover.Trigger asChild>
+          <button
+            className={styles['user-button']}
+            aria-label="User management"
+            type="button"
+          >
+            <Image
+              alt={`${firstName} ${lastName}`}
+              height={32}
+              priority={true}
+              src={user.imageUrl}
+              width={32}
+            />
+          </button>
+        </Popover.Trigger>
+        <Popover.Portal>
+          <Popover.Content
+            align="end"
+            className={styles['user-popover']}
+            collisionPadding={16}
+            side="bottom"
+            sideOffset={8}
+          >
+            <Link
+              className={styles['user-profile-link']}
+              href={`/${user.username}`}
+            >
+              <span className={styles['user-avatar']}>
+                <Image
+                  alt={`${firstName} ${lastName}`}
+                  height={44}
+                  src={user.imageUrl}
+                  width={44}
+                />
+              </span>
+              <span className={styles['user-preview-text']}>
+                <p
+                  className={styles['primary-user-identifier']}
+                >{`${firstName} ${lastName}`}</p>
+                <p>{user.username}</p>
+              </span>
+            </Link>
+
+            <button
+              className={styles['manage-account-button']}
+              onClick={() => openUserProfile()}
+              type="button"
+              tabIndex={-1}
+            >
+              <svg height={12} width={12}>
+                <use href="/icons.svg#gear" />
+              </svg>{' '}
+              Manage account
+            </button>
+            <SignOutButton signOutOptions={{ sessionId: session?.id }}>
+              <button
+                className={styles['sign-out-button']}
+                type="button"
+                tabIndex={-1}
+              >
+                <svg height={12} width={12}>
+                  <use href="/icons.svg#signOut" />
+                </svg>
+                Sign out
+              </button>
+            </SignOutButton>
+
+            <footer>
+              <p>
+                Secured by{' '}
+                <a
+                  href="http://www.clerk.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <svg height={14} width={48}>
+                    <use href="/icons.svg#clerkLogo" />
+                  </svg>
+                </a>
+              </p>
+            </footer>
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover.Root>
+    </ClerkLoaded>
+  );
+};
 
 export function Header() {
-  const pathname = usePathname();
   return (
     <header className={styles.content}>
       <Link className={styles['logo-mark']} href="/">
@@ -18,7 +122,7 @@ export function Header() {
       </Link>
       <span className={styles['user-controls']}>
         <SignedIn>
-          <UserButton afterSignOutUrl={pathname} />
+          <CustomUserButton />
         </SignedIn>
         <SignedOut>
           <SignUpButton mode="modal">
