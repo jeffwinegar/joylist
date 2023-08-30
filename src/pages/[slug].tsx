8b1702ac07b986c829152401ca2dc28ed058cedb
Jeff Winegar <winegarj@gmail.com>
2023-08-30 16:31:44 -0700

    fix: revert test ios not showing kebab icon

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -225,19 +225,11 @@ const BusinessViewContent = (props: BusinessWithUser) => {
       </p>
       <span className={styles['list-icon']}>
         {isUserProfile ? (
-          <button
-            className={styles['entry-dropdown-button']}
-            aria-label="Entry management"
-          >
-            <svg height={20} width={20}>
-              <use href="/icons.svg#kebab" />
-            </svg>
-          </button>
+          <EntryManagePopover
+            businessId={business.id}
+            businessUrl={business.url}
+          />
         ) : (
-          // <EntryManagePopover
-          //   businessId={business.id}
-          //   businessUrl={business.url}
-          // />
           <svg height={20} width={20}>
             <use href="/icons.svg#arrowUpRight" />
           </svg>
